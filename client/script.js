84e4fc7f049be257ea0d4e35917ce5c050f09b58
arunyadav <arunyadav838481@gmail.com>
2023-01-11 21:37:19 +0400

    update the server url

--- client/script.js
+++ client/script.js
@@ -65,7 +65,7 @@ const handleSubmit= async (e)=>{
 
   loader(messageDiv);
 
-  const response=await fetch('http://localhost:5000',{
+  const response=await fetch('https://chatapp-ge3a.onrender.com',{
   method:'POST',
   headers:{
     'Content-Type': 'application/json'
